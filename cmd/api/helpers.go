34aae0289695323eb05e033ebed1bc3002abbd8b
Erik Tank <erik.tank@ticketmaster.com>
2022-08-07 12:35:36 -0700

    Chapter 3.4 - Enveloping JSON

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -29,18 +29,23 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	return id, nil
 }
 
+// Define an envelope type.
+type envelope map[string]any
+
 // Define a writeJSON() helper for sending responses. This takes the destination
 // http.ResponseWriter, the HTTP status code to send, the data to encode to JSON, and a
 // header map containing any additional HTTP headers we want to include in the response.
-func (app *application) writeJSON(w http.ResponseWriter, status int, data any, headers http.Header) error {
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
 	// Encode the data to JSON, returning the error if there was one.
+	//   make it look nice for command line UIs
+	// js, err := json.MarshalIndent(data, "", "\t")
 	js, err := json.Marshal(data)
 	if err != nil {
 		return err
 	}
 
 	// Append a newline to make it easier to view in terminal applications.
-	js = append(js, '\n')
+	// js = append(js, '\n')
 
 	// At this point, we know that we won't encounter any more errors before writing the
 	// response, so it's safe to add any headers that we want to include. We loop
